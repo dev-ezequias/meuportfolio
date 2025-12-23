35611fd2b2913da16169ee8ea3a9cd7ba7316132
Ezequias da Silva Cordeiro <ezequias.sccontato@icloud.com>
2025-12-23 16:52:09 -0300

    Só falta a section formação, depois é só deixar responsivo.

--- index.js
+++ index.js
@@ -25,3 +25,35 @@ document.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', ativarAba);
     });
 });
+
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+  // Se rolar mais de 300px, mostra o botão
+  if (window.scrollY > 300) {
+    btnTopo.classList.add("show");
+  } else {
+    btnTopo.classList.remove("show");
+  }
+});
+
+// Opcional: Garante que o clique leve ao topo mesmo em navegadores antigos
+btnTopo.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+function enviar(event) {
+  event.preventDefault()
+  if (document.getElementById('texto').value && document.getElementById('email').value.includes('@') && document.getElementById('name').value){
+    let enviar = document.getElementById('btn-submit')
+
+      enviar.innerHTML = 'Enviado!' 
+  }
+  else{
+    alert('Por favor, preencha todos os campos corretamente!')
+  }
+}
